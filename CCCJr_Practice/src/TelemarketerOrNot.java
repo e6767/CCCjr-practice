7499691b74a92383f92f32100c9fc39545662ada
Eddie <edwardji3010@gmail.com>
2026-02-09 19:48:35 -0800

    CCC 2018 j1

---
+++ CCCJr_Practice/src/TelemarketerOrNot.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class TelemarketerOrNot {
+    public static void main (String [] args){
+        Scanner scanner = new Scanner(System.in);
+        boolean telemarketer = false;
+        boolean telemarketer2 = false;
+        int sameNumber = 0;
+        boolean numberSame = false;
+        for(int i = 0; i < 4; i++){
+            int number = scanner.nextInt();
+            scanner.nextLine();
+            if(i == 0 && (number == 8 || number == 9)){
+                telemarketer = true;
+            }
+            if(i == 1){
+                sameNumber = number;
+            }
+            if(i == 2 && number == sameNumber){
+                telemarketer = true;
+                numberSame = true;
+            }
+            else{
+                telemarketer = false;
+            }
+            if (i == 3 && (number == 8 || number == 9)){
+                if(numberSame){
+                    telemarketer2 = true;
+                }
+            }
+        }
+        if(telemarketer == true && telemarketer2 == true){
+            System.out.println("ignore");
+        }
+        else{
+            System.out.println("answer");
+        }
+    }
+}
